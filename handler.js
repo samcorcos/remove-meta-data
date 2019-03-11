5faf3143f3e74f1640f2f30d866cb97f74450d91
xxxx <sam@corcos.io>
2019-03-11 18:05:55 -0400

    update docs

--- handler.js
+++ handler.js
@@ -1,6 +1,24 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+/**
+
+  NOTE This is how I do it locally. Keep in mind that after you remove the metadata
+  (which is what exiftool is doing), you also have to flatten it because metadata
+  tags are reversible (which is what qpdf is doing).
+
+  const shell = require('shelljs')
+  const file = process.argv[2]
+
+  // This works by removing the data, then flattening the PDF to make the changes
+  // irreversible (otherwise, tags are reversible)
+
+  shell.exec(`exiftool -all:all= ${file}`)
+  shell.exec(`qpdf ${file} _${file}`)
+
+ */
+
+module.exports.removeMetaData = async (event, context) => {
+  // TODO do some processing here
   return {
     statusCode: 200,
     body: JSON.stringify({
